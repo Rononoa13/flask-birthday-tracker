48ea7146ae307855eda5a35479f4b6c63318ba13
Rononoa13 <sumitluitel42@gmail.com>
2023-01-23 21:48:03 +0545

    Added test and update requirements

---
+++ test.py
@@ -0,0 +1,13 @@
+from app import app
+
+def test1():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+
+def test2():
+    response = app.test_client().get("/")
+    assert b"Birthdays" in response.data
+    assert b"Add a Birthday" in response.data
+    assert b"Add Birthday" in response.data
+    assert b"All Birthdays" in response.data
+    assert b"Delete" in response.data
