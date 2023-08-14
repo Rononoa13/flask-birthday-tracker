4636a1b63b74136d9b2c8f4b3bc0e91df5e4eb15
Rononoa13 <sumitluitel42@gmail.com>
2023-08-14 22:08:41 +0545

    post requtest

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,10 +4,13 @@ def test_home_route_returns_200():
     response = app.test_client().get("/")
     assert response.status_code == 200
 
-def test_home_route_content():
+
+def test_home_route_contains_birthday_related_content():
     response = app.test_client().get("/")
-    assert b"Birthdays" in response.data
-    assert b"Add a Birthday" in response.data
-    assert b"Add Birthday" in response.data
-    assert b"All Birthdays" in response.data
+    content = response.data.decode("utf-8")
+    
+    assert f"Birthdays" in content
+    assert f"Add a Birthday" in content
+    assert f"Add Birthday" in content
+    assert f"All Birthdays" in content
 
